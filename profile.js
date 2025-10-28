66777f010ae747aaf50ca13410564ac316be8d3b
22842 <ostaptuhaj@gmail.com>
2025-10-28 19:23:28 +0200

    htmmmmml

--- profile.js
+++ profile.js
@@ -3,3 +3,11 @@ const favoritePosts = document.querySelector(".favorite_post");
 const likedPosts = document.cookie
   .split("; ")
   .filter((row) => row.startsWith("liked_posts"))
+
+  likedPosts.forEach((post) => {
+  const postId = post.split("=")[1];
+
+  const img = document.createElement("img");
+  img.src = `img/photo${postId}.png`;
+  favoritePosts.appendChild(img);
+});
